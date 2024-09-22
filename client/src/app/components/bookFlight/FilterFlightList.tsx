7e768e51be5fb50b6e862cbcc5acc86b8db3d38d
bberkaykarademir <bberkaykarademir@hotmail.com>
2024-09-22 19:42:50 +0300

    feat(client): update FilterFlightList

--- client/src/app/components/bookFlight/FilterFlightList.tsx
+++ client/src/app/components/bookFlight/FilterFlightList.tsx
@@ -1,18 +1,41 @@
 import React from "react";
 
 const FilterFlightList = () => {
+  const airlines = [
+    "American Airlines",
+    "British Airways",
+    "Air France",
+    "KLM",
+    "Emirates",
+    "Qatar Airways",
+    "Lufthansa",
+    "Singapore Airlines",
+    "Qantas",
+    "Cathay Pacific",
+  ];
+
+  const sortOptions = [
+    { name: "Lowest Price", value: "lowestPrice" },
+    { name: "Highest Price", value: "highestPrice" },
+    { name: "Earliest Departure", value: "+scheduleTime" },
+    { name: "Latest Departure", value: "-scheduleTime" },
+  ];
+
   return (
-    <section className="flex flex-col gap-7 pt-4">
-      <div className="flex flex-col gap-4">
+    <section className="flex flex-col gap-6 pt-2 text-sm">
+      <div className="flex flex-col gap-3">
         <span className="font-bold">Sort by</span>
         <select className="w-56 h-9 flex inputWrapper-focus px-2 bg-background-light rounded-md">
-          <option selected>Lowest Price</option>
-          <option>Highest Price</option>
+          {sortOptions.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.name}
+            </option>
+          ))}
         </select>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-3">
         <span className="font-bold">Arrival Time</span>
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-[10px]">
           <li className="flex items-center gap-2">
             <input type="radio" id="am" name="times" />
             <label htmlFor="am">5:00 AM - 11:59 AM</label>
@@ -23,51 +46,41 @@ const FilterFlightList = () => {
           </li>
         </ul>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-3">
+        <span className="font-bold">Stops</span>
+        <ul className="flex flex-col gap-[10px]">
+          <li className="flex items-center gap-2">
+            <input type="radio" id="nonstop" name="stops" />
+            <label htmlFor="nonstop">Nonstop</label>
+          </li>
+          <li className="flex items-center gap-2">
+            <input type="radio" id="oneStop" name="stops" />
+            <label htmlFor="oneStop">1 Stop</label>
+          </li>
+          <li className="flex items-center gap-2">
+            <input type="radio" id="moreStop" name="stops" />
+            <label htmlFor="moreStop">2+ Stop</label>
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col gap-3">
         <span className="font-bold">Airlines Included</span>
-        <ul className="flex flex-col gap-4">
-          <li className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <input type="radio" id="alitalia" name="airlines" />
-              <label htmlFor="alitalia">Alitalia</label>
-            </div>
-            <span>$230</span>
-          </li>
-          <li className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <input type="radio" id="lufthansa" name="airlines" />
-              <label htmlFor="lufthansa">Lufthansa</label>
-            </div>
-            <span>$230</span>
-          </li>
-          <li className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <input type="radio" id="air-france" name="airlines" />
-              <label htmlFor="air-france">Air France</label>
-            </div>
-            <span>$230</span>
-          </li>
-          <li className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <input type="radio" id="brussels-airlines" name="airlines" />
-              <label htmlFor="brussels-airlines">Brussels Airlines</label>
-            </div>
-            <span>$230</span>
-          </li>
-          <li className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <input type="radio" id="air-italy" name="airlines" />
-              <label htmlFor="air-italy">Air Italy</label>
-            </div>
-            <span>$230</span>
-          </li>
-          <li className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <input type="radio" id="siberia" name="airlines" />
-              <label htmlFor="siberia">Siberia</label>
-            </div>
-            <span>$230</span>
-          </li>
+        <ul className="flex flex-col gap-[10px] max-h-52 overflow-y-auto">
+          {airlines.map((airline, index) => (
+            <li key={index} className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  id={airline.toLowerCase().replace(/\s+/g, "-")}
+                  name="airlines"
+                />
+                <label htmlFor={airline.toLowerCase().replace(/\s+/g, "-")}>
+                  {airline}
+                </label>
+              </div>
+              <span>$230</span>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
