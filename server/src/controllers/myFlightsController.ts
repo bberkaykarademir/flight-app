5e19be14db84f6f691580458bcfaa53140ff8c4f
bberkaykarademir <bberkaykarademir@hotmail.com>
2024-09-23 23:59:48 +0300

    feat(server): add validation for booking a flight
    - add validation to check if the flight is already booked or if it has already departed before booking.
    - return appropriate error messages for each scenario

--- server/src/controllers/myFlightsController.ts
+++ server/src/controllers/myFlightsController.ts
@@ -26,12 +26,27 @@ export const getFlights = async (req: Request, res: Response) => {
 };
 
 export const bookFlight = async (req: Request, res: Response) => {
-  const flight = req.body;
-  const newFlight = new Flight(flight);
+  const { flightNumber, departureDateTime } = req.body;
+
   try {
+    const existingFlight = await Flight.findOne({ flightNumber });
+
+    if (existingFlight) {
+      return res.status(409).json({ message: "Flight already booked." });
+    }
+
+    const currentDateTime = new Date();
+    const flightDepartureTime = new Date(departureDateTime);
+
+    if (flightDepartureTime < currentDateTime) {
+      return res
+        .status(400)
+        .json({ message: "Can not book a flight that has already departed." });
+    }
+    const newFlight = new Flight(req.body);
     await newFlight.save();
     res.status(201).json(newFlight);
   } catch (error) {
-    res.status(409).json({ message: (error as Error).message });
+    res.status(500).json({ message: (error as Error).message });
   }
 };
